a05cc646fd5caf06343d74a666ce8af065d164d6
mani-vd <manikandanvd123@gmail.com>
2024-03-30 19:21:15 +0530

    Added back button toast, migrated startactivityforresult method, migrated onbackpressedmethod

--- app/src/main/java/my/status/details/StartActivity.java
+++ app/src/main/java/my/status/details/StartActivity.java
@@ -1,5 +1,8 @@
 package my.status.details;
-import androidx.annotation.Nullable;
+import androidx.activity.OnBackPressedCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
@@ -10,6 +13,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.window.OnBackInvokedCallback;
+import android.window.OnBackInvokedDispatcher;
 
 import com.jmedeisis.draglinearlayout.DragLinearLayout;
 
@@ -18,13 +23,64 @@ import java.util.Locale;
 import java.util.Objects;
 
 public class StartActivity extends AppCompatActivity {
-    private ImageView iv_mic;
     private TextView tv_Speech_to_text;
-    private static final int REQUEST_CODE_SPEECH_INPUT = 1;
+    private long pressedTime;
+    Intent google_intent;
+    private ActivityResultLauncher<Intent> google_launcher;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+        google_launcher = registerForActivityResult(
+                new ActivityResultContracts.StartActivityForResult(),
+                result -> {
+
+                    Intent data=result.getData();
+                    if(data!=null){
+                        ArrayList<String> resultString = data.getStringArrayListExtra(
+                                RecognizerIntent.EXTRA_RESULTS);
+                        tv_Speech_to_text.setText(
+                                Objects.requireNonNull(resultString).get(0));
+                    }
+
+
+                }
+        );
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.TIRAMISU) {
+            new OnBackInvokedDispatcher() {
+                @Override
+                public void registerOnBackInvokedCallback(int priority, @NonNull OnBackInvokedCallback callback) {
+                    if (pressedTime + 2000 > System.currentTimeMillis()) {
+                    finish();
+                    } else {
+                        Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+                    }
+                    pressedTime = System.currentTimeMillis();
+                }
+
+                @Override
+                public void unregisterOnBackInvokedCallback(@NonNull OnBackInvokedCallback callback) {
+
+                }
+
+            };
+        }
+        else{
+            getOnBackPressedDispatcher().addCallback(this, new OnBackPressedCallback(true) {
+                @Override
+                public void handleOnBackPressed() {
+                    if (pressedTime + 2000 > System.currentTimeMillis()) {
+                    finish();
+                    } else {
+                        Toast.makeText(getBaseContext(), "Press back again to exit", Toast.LENGTH_SHORT).show();
+                    }
+                    pressedTime = System.currentTimeMillis();
+                }
+            });
+        }
+
 
         //Draglinearlayout start
         DragLinearLayout dragLayout = findViewById(R.id.container);
@@ -35,7 +91,7 @@ public class StartActivity extends AppCompatActivity {
 
             // below is the child inside dragger layout
             View sourceElem = dragLayout.getChildAt(i);
-            View destElem=null;
+            View destElem;
             if(i==0){
                 destElem=dragLayout.getChildAt(dragLayout.getChildCount()-1);
             }
@@ -53,64 +109,46 @@ public class StartActivity extends AppCompatActivity {
 
         // Refresh  the layout
         swipeRefreshLayout.setOnRefreshListener(
-                new SwipeRefreshLayout.OnRefreshListener() {
-                    @Override
-                    public void onRefresh() {
+                () -> {
 
-                        // Your code goes here
-                        // In this code, we are just
-                        // changing the text in the textbox
-                        textView.setText("Refreshed");
+                    // Your code goes here
+                    // In this code, we are just
+                    // changing the text in the textbox
+                    textView.setText(R.string.refreshed);
 
-                        // This line is important as it explicitly
-                        // refreshes only once
-                        // If "true" it implicitly refreshes forever
-                        swipeRefreshLayout.setRefreshing(false);
-                    }
+                    // This line is important as it explicitly
+                    // refreshes only once
+                    // If "true" it implicitly refreshes forever
+                    swipeRefreshLayout.setRefreshing(false);
                 }
         );
 
         //speech to text start
-        iv_mic = findViewById(R.id.iv_mic);
+        ImageView iv_mic = findViewById(R.id.iv_mic);
         tv_Speech_to_text = findViewById(R.id.tv_speech_to_text);
 
-        iv_mic.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v)
-            {
-                Intent intent
-                        = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
-                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
-                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
-                        Locale.getDefault());
-                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text");
+        iv_mic.setOnClickListener(v -> {
+            google_intent
+                    = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            google_intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            google_intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                    Locale.getDefault());
+            google_intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text");
 
-                try {
-                    startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
-                }
-                catch (Exception e) {
-                    Toast
-                            .makeText(StartActivity.this, " " + e.getMessage(),
-                                    Toast.LENGTH_SHORT)
-                            .show();
-                }
+            try {
+//                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+
+
+                google_launcher.launch(google_intent);
+            }
+            catch (Exception e) {
+                Toast
+                        .makeText(StartActivity.this, " " + e.getMessage(),
+                                Toast.LENGTH_SHORT)
+                        .show();
             }
         });
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    @Nullable Intent data)
-    {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
-            if (resultCode == RESULT_OK && data != null) {
-                ArrayList<String> result = data.getStringArrayListExtra(
-                        RecognizerIntent.EXTRA_RESULTS);
-                tv_Speech_to_text.setText(
-                        Objects.requireNonNull(result).get(0));
-            }
-        }
-    }
 }
